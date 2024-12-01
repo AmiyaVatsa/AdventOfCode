f40a49f4586ede5d64bc47334dca8d1aa8493fc0
AmiyaVatsa <amiyavatsa1807@gmail.com>
2024-12-01 11:00:08 +0530

    add day-2 template files

---
+++ 2024/day2/main.py
@@ -0,0 +1,27 @@
+import pathlib
+import sys
+
+def parse(puzzle_input):
+	print(puzzle_input)
+	return ""
+
+def part_1_solution(data):
+	return ""
+
+def part_2_solution(data):
+	return ""
+
+def solve(input):
+	data = parse(puzzle_input)
+	solution_1 = part_1_solution(data)
+	solution_2 = part_2_solution(data)
+
+	return solution_1, solution_2
+
+if __name__ == '__main__':
+	for path in sys.argv[1:]:
+		print(f"{path}:")
+		puzzle_input = pathlib.Path(path).read_text().strip()
+		solutions = solve(puzzle_input)
+		print("\n".join(str(solution) for solution in solutions))
+
