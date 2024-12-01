2d45eb52e4db8d501c346ccaac3a4c28aa100463
AmiyaVatsa <amiyavatsa1807@gmail.com>
2024-12-01 10:53:56 +0530

    add day-1 solution

--- 2024/day1/main.py
+++ 2024/day1/main.py
@@ -2,13 +2,47 @@ import pathlib
 import sys
 
 def parse(puzzle_input):
-	return ""
+	puzzle_input += '\n'
+	#print(puzzle_input)
+	list1 = []
+	list2 = []
+	num = ""
+	for i in puzzle_input:
+		if i != ' ' and i != '\n':
+			num += i
+		elif i == ' ' and num.isnumeric():
+			list1.append(int(num))
+			num = ""
+		elif i == '\n' and num.isnumeric():
+			list2.append(int(num))
+			num = ""
+	#print(list1)
+	#print(list2)
+	return list1, list2
 
 def part_1_solution(data):
-	return ""
+	list1, list2 = data
+	list1.sort()
+	list2.sort()
+	ans = 0
+	for i in range(len(list1)):
+		ans += abs(list1[i] - list2[i])
+	return ans
 
 def part_2_solution(data):
-	return ""
+	list1, list2 = data
+	dict_list_2 = {}
+	ans = 0
+	for i in list2:
+		if i in dict_list_2:
+			dict_list_2[i] += 1
+		else:
+			dict_list_2[i] = 1
+	for i in list1:
+		if (i not in dict_list_2):
+			continue
+		ans += (i * dict_list_2[i])
+	return ans
 
 def solve(input):
 	data = parse(puzzle_input)
