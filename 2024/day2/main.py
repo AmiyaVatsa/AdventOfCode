26b712fb33d1673f399af250045f0c9061d1954d
AmiyaVatsa <amiyavatsa1807@gmail.com>
2024-12-02 11:04:54 +0530

    add day-2 solution

--- 2024/day2/main.py
+++ 2024/day2/main.py
@@ -2,14 +2,70 @@ import pathlib
 import sys
 
 def parse(puzzle_input):
-	print(puzzle_input)
-	return ""
+	puzzle_input += '\n'
+	#print(puzzle_input)
+	levels = []
+	temp = []
+	nums = ""
+	for i in puzzle_input:
+		if i == ' ':
+			temp.append(int(nums))
+			nums = ""
+		elif i == '\n':
+			temp.append(int(nums))
+			levels.append(temp)
+			temp = []
+			nums = ""
+		else:
+			nums += i
+	#print(levels)
+	return levels
 
 def part_1_solution(data):
-	return ""
+	#print(data)
+	safe = 0
+	for i in data:
+		flag = False
+		for j in range(1, len(i) - 1):
+			if (i[j] <= i[j - 1] and i[j] <= i[j + 1]) or (i[j] >= i[j - 1] and i[j] >= i[j + 1]):
+				flag = True
+				break
+		for j in range(len(i) - 1):
+			if (abs(i[j] - i[j + 1]) >= 1 and abs(i[j] - i[j + 1]) <= 3):
+				continue
+			else:
+				flag = True
+				break
+		if (not flag):
+			safe += 1
+	return safe
 
 def part_2_solution(data):
-	return ""
+	safe = 0
+	for i in data:
+		damp = False
+		flag = False
+		for j in range(1, len(i) - 1):
+			if (i[j] <= i[j - 1] and i[j] <= i[j + 1]) or (i[j] >= i[j - 1] and i[j] >= i[j + 1]):
+				if (not damp):
+					damp = True
+					#print(i)
+					continue
+				flag = True
+				break
+		for j in range(len(i) - 1):
+			if (abs(i[j] - i[j + 1]) >= 1 and abs(i[j] - i[j + 1]) <= 3):
+				continue
+			else:
+				if not damp:
+					damp = True
+					#print(i)
+					continue
+				flag = True
+				break
+		if (not flag):
+			safe += 1
+	return safe
 
 def solve(input):
 	data = parse(puzzle_input)
